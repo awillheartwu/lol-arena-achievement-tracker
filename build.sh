1e65d5537c70ad22b930e4f1dd43e859800b6a0d
吴浩李 <wuhaoli@WuMbpM4pro48G.lan>
2025-09-16 03:25:03 +0800

    fix build

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ echo "building with tag: $TAG"
 # 构建前端项目
 pnpm build
 
+# 修复 dist 权限
+chmod -R 755 dist
+
 # 构建 Docker 镜像（版本号 & latest）
 sudo docker build -f docker/Dockerfile -t lol-arena:latest -t lol-arena:$TAG .
